e3ee0d722c0800f70ef8a325039959a008537194
clairtonluz <clairton.c.l@gmail.com>
2015-05-30 10:20:02 -0300

    criando model

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var path = require('path')
 var express = require('express');
 var app = express();
-var pg = require('pg');
+//var pg = require('pg');
+var sequelize = new Sequelize(process.env.DATABASE_URL);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -12,17 +13,18 @@ app.set('view engine', 'jade')
 // routes ======================================================================
 require('./app/routes.js')(app);
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-})
+//app.get('/db', function (request, response) {
+//  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//    client.query('SELECT * FROM COMPANY', function(err, result) {
+//      done();
+//      if (err)
+//       { console.error(err); response.send("Error " + err); }
+//      else
+//       { response.send(result.rows); }
+//    });
+//  });
+//})
+
 
 app.listen(app.get('port'), function() {
   console.log('SysProv is running on port', app.get('port'));
