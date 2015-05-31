1f6ba39d69efd32cd52eda3081bb26bb5e748252
clairtonluz <clairton.c.l@gmail.com>
2015-05-31 09:33:34 -0300

    implementing auto create entities

--- db/initDB.js
+++ db/initDB.js
@@ -1,12 +1,24 @@
+var pathUtil = require('../app/util/pathUtil');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(process.env.DATABASE_URL);
-
-var models = ['user'];
+const modelsDir = __dirname + "/../model/"
 
 module.exports = function() {
-    models.forEach(function(model){
-        sequelize.import(__dirname + "/../model/" + model);
-        sequelize.sync({/*force: true, */logging: null});
-        console.log("[SEQUELIZE] CREATING ENTITY " + model);
+    pathUtil.walk(modelsDir, function(err, models) {
+      if (err) throw err;
+
+      models.forEach(function(model){
+          sequelize.import(model);
+          console.log("[SEQUELIZE] CREATING ENTITY " + model);
+      });
+
+      sequelize.sync({/*force: true,*/ logging: null});
+
     });
 }
+
+module.exports.import = function(model) {
+    return sequelize.import(modelsDir + model);
+}
+
+
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var path = require('path')
 var express = require('express');
 var bodyparser = require('body-parser')
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+var routes = require('./app/routes.js');
+var db = require('./db/initDB');
+
+app.use(express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || 8080));
 app.use(bodyparser.urlencoded({extended: false}))
 
 // routes ======================================================================
-require('./app/routes.js')(app);
+routes(app);
 
-require('./db/initDB')()
+// create table of database ====================================================
+db();
+//db.import('user');
 
 app.listen(app.get('port'), function() {
   console.log('SysProv is running on port', app.get('port'));
